516332b152bb60895b29b3c9147e9f4ac6b5d0cb
sarwar0172 <sarwarhossain017244@gmail.com>
2025-08-04 09:31:30 +0600

    sendOTp verifyOTP

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,6 +1,9 @@
 const OtpGenerate = require("../utils/otpGenerator");
 const User = require("../models/user");
 const response = require("../utils/responsehandler");
+const sendOtpTopEmail = require("../services/emailservice");
+const twilloService = require("../services/twilloService");
+const generateToken = require("../utils/generateToken");
 
 // send-1 send otp
 const sendOtp=async(req,res)=>{
@@ -18,6 +21,7 @@ const sendOtp=async(req,res)=>{
             user.emailOtp=otp;
             user.emailOtpExpiry=expiry;
             await user.save();
+            await sendOtpTopEmail(email,otp);
 
             response(res,{email},200,"Otp sent successfully");
         }
@@ -31,7 +35,7 @@ const sendOtp=async(req,res)=>{
         }
         
         await user.save();
-
+        twilloService.sendOtpToPhoneNumber(fullphoneNumber,otp);
         response(res,user,200,"Otp sent successfully");
     }catch(err){
         console.log(err);
@@ -41,3 +45,53 @@ const sendOtp=async(req,res)=>{
 }
 
 
+// verify otp
+const verifyOtp=async(req,res)=>{
+    const {phoneNumber,phoneSuffix,email,otp}=req.body;
+    let user;
+    try{
+        if(email){
+             user=await User.findOne({email});
+            if(!user){
+                return response(res,null,400,"User not found");
+            }
+            if(user.emailOtp!==otp || user.emailOtpExpiry<new Date()){
+                return response(res,null,400,"Invalid otp");
+            }
+            user.emailOtp=null;
+            user.emailOtpExpiry=null;
+            user.isVerfied=true;
+            await user.save();
+            response(res,user,200,"Email verified successfully");
+        }else{
+        if(!phoneNumber || !phoneSuffix){
+            return response(res,null,400,"Please enter phone number and suffix");
+        }
+        const fullphoneNumber=`${phoneSuffix}${phoneNumber}`;
+         user=await User.findOne({phoneNumber});
+        if(!user){
+            return response(res,null,400,"User not found");
+        }
+        const isValid=await twilloService.verifyPhoneNumber(fullphoneNumber,otp);
+        if(!isValid){
+            return response(res,null,400,"Invalid otp");
+        }
+        user.isVerfied=true;
+        await user.save();
+    }
+
+    const token=generateToken(user?._id);
+    res.cookie("token",token,{httpOnly:true});
+
+   return response(res,{token,user},200,"Phone number verified successfully");
+    }catch(err){
+        console.log(err);
+        return response(res,null,400,"Something went wrong while verifying otp");
+    }
+}
+
+
+module.exports={
+    sendOtp,
+    verifyOtp
+}
---
+++ backend/utils/generateToken.js
@@ -0,0 +1,8 @@
+const jwt=require('jsonwebtoken')
+
+const generateToken=(userId)=>{
+    const token=jwt.sign({userId},process.env.JWT_SECRET,{expiresIn:'1y'})
+    return token
+}
+
+module.exports=generateToken
